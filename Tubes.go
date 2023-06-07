14ed7027f4215cdc46061d3d181e8aeba4c81c77
Han25 <farhaneditya03@gmail.com>
2023-06-08 00:09:57 +0700

    konsep awal

--- Tubes.go
+++ Tubes.go
@@ -2,8 +2,53 @@ package main
 
 import "fmt"
 
+type user struct {
+	nama    string
+	keluhan string
+}
+
+type dokter struct {
+}
+
+func header() {
+
+	fmt.Println("------------------------------------------------")
+	fmt.Println("       Selamat datang di aplikasi konsultasi")
+	fmt.Println("              Kesehatan Online")
+	fmt.Println("------------------------------------------------")
+	fmt.Println()
+	fmt.Println("                    - - - - - -")
+	fmt.Println("     Kami di sini untuk menjaga kesehatan Anda!")
+	fmt.Println("                    - - - - - -")
+	fmt.Println()
+
+	Menu()
+
+}
+
+func Menu() {
+	var choose int
+	fmt.Print("Silahkan masukkan keperluan anda: \n")
+
+	fmt.Println("1. Registrasi")
+	fmt.Println("2. Login")
+	fmt.Println("3. Lihat Masalah")
+	fmt.Println("4. Quit")
+
+	fmt.Scan(&choose)
+
+	switch choose {
+	case 1:
+		fmt.Println("Silahkan Registrasi terlebih dahulu")
+	case 2:
+		fmt.Println("Silahkan Login")
+	case 3:
+		fmt.Println("Sampai jumpa lagi")
+	}
+
+}
 func main() {
 
-	fmt.Println("halo ges")
-	fmt.Println("Semoga cepat selesai ini tubes")
+	header()
+
 }
