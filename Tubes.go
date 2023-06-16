2cb4a50f0b67f444c2271c5e4cf5ccf1c27f5368
Han25 <farhaneditya03@gmail.com>
2023-06-16 23:48:38 +0700

    add selection sort as function

--- Tubes.go
+++ Tubes.go
@@ -11,20 +11,23 @@ type user struct {
 }
 
 type question struct {
-	tag       string
-	keluhan   string
-	jumlahTag int
+	tag           string
+	keluhan       string
+	jumlahTag     int
+	balasanDokter string
 }
 
 type arrUser [NMAX]user
 type arrQuestion [NMAX]question
 
 func header(T *arrUser) {
-	var A arrQuestion
-	var n int
+
 	fmt.Println("------------------------------------------------")
 	fmt.Println("       Selamat datang di aplikasi konsultasi")
 	fmt.Println("              Kesehatan Online")
+	fmt.Println("                 Created By :                                ")
+	fmt.Println("           Muhammad Farhan Editya")
+	fmt.Println("                 Rakha Bayu               ")
 	fmt.Println("------------------------------------------------")
 	fmt.Println()
 	fmt.Println("                    - - - - - -")
@@ -32,7 +35,6 @@ func header(T *arrUser) {
 	fmt.Println("                    - - - - - -")
 	fmt.Println()
 
-	Menu(T, A, n)
 }
 
 func Menu(T *arrUser, A arrQuestion, n int) {
@@ -46,6 +48,7 @@ func Menu(T *arrUser, A arrQuestion, n int) {
 	fmt.Println("3. Masuk sebagai dokter")
 	fmt.Println("4. Lihat forum")
 	fmt.Println("5. Cari pertanyaan")
+	fmt.Print("Pilihan: ")
 
 	fmt.Scan(&choose)
 
@@ -54,21 +57,23 @@ func Menu(T *arrUser, A arrQuestion, n int) {
 		register(T)
 
 	case 2:
-		if login(T) {
+		if login(*T) {
 			postQuestion(&A, &n)
 		} else {
 			fmt.Println("Login gagal!")
 			viewForum(A, n)
 		}
 	case 3:
-		sortTag(&A, n)
-		viewTag(A, n)
+		MenuDoctor(&A, n)
+
 	case 4:
 		viewForum(A, n)
 	case 5:
 		fmt.Print("Masukkan jenis pertanyaan yang akan anda cari: ")
 		fmt.Scan(&topic)
 		findTopic(A, topic, n)
+	case 6:
+		PrintUser(*T, A, n)
 	}
 	Menu(T, A, n)
 }
@@ -101,7 +106,7 @@ func register(T *arrUser) {
 	fmt.Println("Tidak dapat melakukan registrasi. Data sudah penuh.")
 }
 
-func login(T *arrUser) bool {
+func login(T arrUser) bool {
 	var username, password string
 
 	fmt.Println("=== Login ===")
@@ -110,7 +115,7 @@ func login(T *arrUser) bool {
 	fmt.Print("Masukkan password: ")
 	fmt.Scan(&password)
 
-	for i := 0; i < len(*T); i++ {
+	for i := 0; i < len(T); i++ {
 		if T[i].username == username && T[i].password == password {
 			fmt.Println("Login berhasil! ")
 			return true
@@ -126,22 +131,25 @@ func postQuestion(A *arrQuestion, n *int) {
 	fmt.Scan(&pertanyaan)
 	fmt.Print("Masukkan jenis keluhannya: ")
 	fmt.Scan(&tag)
-	*n = 0
 
-	for pertanyaan != "cukup" && tag != "cukup" {
-		A[*n].jumlahTag = countTag(*A, tag, *n)
-		A[*n].keluhan = pertanyaan
-		A[*n].tag = tag
-
-		*n++
-
-		fmt.Print("Mau konsultasi apa? (ketik 'cukup' untuk menghentikan): ")
-		fmt.Scan(&pertanyaan)
-		fmt.Print("Masukkan jenis keluhannya: ")
-		fmt.Scan(&tag)
+	newQuestion := question{
+		keluhan:       pertanyaan,
+		tag:           tag,
+		balasanDokter: "",
 	}
-	fmt.Println("Pertanyaan berhasil diposting!")
+
+	for i := 0; i < len(*A); i++ {
+		if A[i].keluhan == "" && A[i].tag == "" {
+			A[i] = newQuestion
+			*n++
+			fmt.Println("Pertanyaan berhasil diposting!")
+			return
+		}
+	}
+
+	fmt.Println("Kesalahan: Kapasitas tanya sudah penuh!")
 }
+
 func viewForum(A arrQuestion, n int) {
 	if n == 0 {
 		fmt.Println("Belum ada pertanyaan nih")
@@ -151,9 +159,10 @@ func viewForum(A arrQuestion, n int) {
 		fmt.Printf("Pertanyaan %d:\n", i+1)
 		fmt.Println("Keluhan:", A[i].keluhan)
 		fmt.Println("Tag:", A[i].tag)
-
+		if A[i].balasanDokter != "" {
+			fmt.Println("Balasan Dokter:", A[i].balasanDokter)
+		}
 	}
-
 }
 
 func findTopic(A arrQuestion, topic string, n int) {
@@ -173,8 +182,27 @@ func findTopic(A arrQuestion, topic string, n int) {
 	}
 }
 
-func MenuDoctor() {
-	fmt.Print("Selamat datang dokter")
+func MenuDoctor(A *arrQuestion, n int) {
+	var choose int
+	fmt.Println("Selamat datang dokter")
+	fmt.Println("1.Lihat forum ")
+	fmt.Println("2.Balas pertanyaan pasien")
+	fmt.Println("3.Lihat tag penyakit")
+	fmt.Print("Pilihan: ")
+	fmt.Scan(&choose)
+
+	switch choose {
+	case 1:
+		viewForum(*A, n)
+		MenuDoctor(A, n)
+
+	case 2:
+		viewForum(*A, n)
+		replyQuestion(A, n)
+	case 3:
+		sortTag(A, n)
+		viewTag(*A, n)
+	}
 
 }
 
@@ -189,9 +217,15 @@ func countTag(A arrQuestion, tag string, n int) int {
 }
 
 func viewTag(A arrQuestion, n int) {
+	fmt.Println("=== Jumlah Tag ===")
+
 	for i := 0; i < n; i++ {
-		fmt.Println(A[i].tag, A[i].jumlahTag+1)
+
+		tag := A[i].tag
+
+		fmt.Printf("%s: %d\n", tag, countTag(A, A[i].tag, n))
 	}
+
 }
 
 func sortTag(A *arrQuestion, n int) {
@@ -207,8 +241,56 @@ func sortTag(A *arrQuestion, n int) {
 		i++
 	}
 }
+func replyQuestion(A *arrQuestion, n int) {
+	var reply string
+	fmt.Print("Pilih nomor pertanyaan yang ingin Anda balas: ")
+	var pilih int = 0
+	fmt.Scan(&pilih)
+
+	if pilih > 0 && pilih <= n {
+		fmt.Println("Pertanyaan Pasien: ", A[pilih-1].keluhan)
+		fmt.Print("Masukkan balasan Anda: ")
+		fmt.Scan(&reply)
+		A[pilih-1].balasanDokter = reply
+		fmt.Println("Balasan berhasil ditambahkan!")
+	} else {
+		fmt.Println("Nomor pertanyaan tidak valid.")
+	}
+}
+func PrintUser(T arrUser, A arrQuestion, n int) {
+	for i := 0; i < n; i++ {
+		fmt.Println(T)
+	}
+
+	for i := 0; i < n; i++ {
+		fmt.Println(A)
+	}
+}
+
+func selectionSort(A *arrQuestion, n int) {
+	var t question
+	i := 0
+	for i < n-1 {
+		idx_min := i
+		j := i + 1
+		for j < n {
+			if A[j].jumlahTag < A[idx_min].jumlahTag {
+				idx_min = j
+			}
+			j = j + 1
+		}
+		t = A[idx_min]
+		A[idx_min] = A[i]
+		A[i] = t
+		i = i + 1
+	}
+}
+
 func main() {
 	var T arrUser
+	var A arrQuestion
+	var n int
 	header(&T)
+	Menu(&T, A, n)
 
 }
